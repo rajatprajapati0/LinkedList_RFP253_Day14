827850acbc025a3b8780662c5862f09c0c797c3e
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-12 22:55:43 +0530

    Added:Data Structure Problems using C# Generics

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList_Work
+{
+  public class Program
+  {
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("Data Structure\r\nProblems using\r\nC# Generics");
+       
+        
+        
+        
+    }
+
+
+  }
+}
